b55b4a7a06f4517b95f89a51f132fd952ce0ddca
soumyasinghal7732-web <soumyasinghal7732@gmail.com>
2025-09-05 19:30:48 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")   # load home.html from templates folder
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
